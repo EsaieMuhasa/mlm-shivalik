d261c4d4c690152be864b0381a033111b7b0a676
Esaie MUHASA <esaiemuhasa.dev@gmail.com>
2022-01-19 13:19:02 +0200

    Correction de l'utilitaire de redimensionnement des images et de gestionnainer d'ecriture des fichiers uploader sur le serveur

--- PHPBackend/File/FileManager.php
+++ PHPBackend/File/FileManager.php
@@ -21,18 +21,30 @@ class FileManager
      * ecrit un fichier uploder dans le docier publique du serveur
      * @param UploadedFile $file
      * @param string $fileName
+     * @return string chemain absolut vers le fichier sur le serveur
      */
-    public static function writeUploadedFile (UploadedFile $file, ?string $fileName = null) : void {
+    public static function writeUploadedFile (UploadedFile $file, ?string $fileName = null) : string {
         $dir = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.($fileName == null? GlobalConfig::getInstance()->getPublicDirectory() : "Web");
+        
+        $matches = array();
+        $simpleFileName = null;
+        
+        if ($fileName != null && preg_match('`^(.+)(/|\\\\)([a-zA-Z0-9_]+)(\\.[a-zA-Z0-9]{1,6])$`', $fileName)) { //recuprerationd de la hierarchie des dossiers
+            $dir = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.GlobalConfig::getInstance()->getPublicDirectory().DIRECTORY_SEPARATOR."{$matches[1]}";
+            $simpleFileName = "{$matches[2]}{$matches[3]}";
+        }   
         
         if (!is_dir($dir)) {
             @mkdir($dir, 0777, true);
         }
-        $fileName = $dir.DIRECTORY_SEPARATOR.($fileName == null? ($file->getName()) : $fileName);
         
-        if(!(@move_uploaded_file($file->getTmpName(), $fileName))){
+        $reelFileName = $dir.DIRECTORY_SEPARATOR.($fileName == null? ($file->getName()) : ($simpleFileName == null? $fileName : $simpleFileName));
+        
+        if(!(@move_uploaded_file($file->getTmpName(), $reelFileName))){
             throw new PHPBackendException('Erreur de configuration du serveur. tmpfile_name = '.$file->getTmpName().'; filename='.$fileName.'. Echec de recuperation du fichier dans le tmp du serveur');
         }
+        
+        return $reelFileName;
     }
 }
 
--- PHPBackend/Image2D/ImageResizing.php
+++ PHPBackend/Image2D/ImageResizing.php
@@ -5,9 +5,9 @@ namespace PHPBackend\Image2D;
 use PHPBackend\PHPBackendException;
 
 /**
- *
  * @author Esaie Muhasa
- *        
+ * Utilitaire de redimensionnement des images
+ *       
  */
 class ImageResizing extends ImageBuilder
 {
@@ -21,7 +21,7 @@ class ImageResizing extends ImageBuilder
     /**
      * {@inheritDoc}
      * @see ImageBuilder::__construct()
-     * @param Image $images[] collection des carracteristique des image
+     * @param Image[] $images collection des carracteristique des image
      */
     public function __construct(Image $image, array $images)
     {
@@ -102,85 +102,85 @@ class ImageResizing extends ImageBuilder
     /***
      * Generation des images aux dimensions requise
      * @param Image $image
+     * @param ImageRect $image
      * @param bool $remove suprimer le fichier original??/
      * @throws PHPBackendException
      */
-    public static function builder(Image $image, $remove = true) : void{
+    public static function builder(Image $image, array $rects = [], $remove = true) : void{
         //Si l'image d'existe pas physiquement sur le serveur
         if (!$image->fileExists()) {
-            throw new PHPBackendException("Impossible de demanrer les algorithmes des redimensionnement des images. l\'image doit exister physiquement sur le serveur. > {$image->getFileName()} <");
+            throw new PHPBackendException("Impossible de demanrer les algorithmes des redimensionnement des images. l'image doit exister physiquement sur le serveur. > {$image->getFileName()} <");
         }
         
         $matches = array();
-        if (preg_match('#^(.+)-reel\.(png|PNG|jpg|jpeg|JPG|JPEG)$#', $image->getFileName(), $matches)) {
+        if (preg_match('#^(.+)-reel\\.(png|PNG|jpg|JPG|jpeg|JPEG)$#', $image->getFileName(), $matches)) {
             $baseName = $matches[1];
             $extension = $matches[2];
         }else {
             throw new PHPBackendException("impossible de poursuivre les traitements car l'outils de redimensionnement n'arrive pas a separer le nom de l'image a son extension. Image non prise en charge {$image->getFileName()}");
         }
+        
+        //traitement de taille des images
+        /**
+         * @var ImageRect[] $sizes
+         */
+        $sizes = empty($rects) ? [] : $rects;
+        
+        
+        if (empty($sizes)) {
+            /**
+             * @var Image[] $images
+             */
+            $images = [];//informations des images a redimensionner
             
-        /*
-         * \\pour la taille normale
-         * =====================================================
-         * Pour la photo de dimension requise, un copression simple est naicessaire
-         */
-        $sizeNormale = ImageRect::getDefaultSize();
-        $imageName = $baseName.'.'.$extension;//le nom de l'image de base
-        $destination = new Image($imageName, $sizeNormale);
-        
-        self::equalize($image, $destination);
-        
-        
-        /*
-         * \\Pour les images minuatures
-         * ================================================================
-         */
-        $sizeMin = ImageRect::getDefaultMinuatureSize();
-        $imageMinName = $baseName.'-min.'.$extension;
-        $min_destination = new Image($imageMinName, $sizeMin);
-        self::equalize($image, $min_destination);
-        
-        
-        
-        /*
-         * \\Pour les thumbs
-         * ======================================================
-         */
-        
-        /*
-         * Creation du thumb normal
-         * ----------------------------
-         * Le principe de base est de calculer le pourcentage de la hoteur de l'image, qui veau 200px
-         */
-        $to_pourcent_200px = (100 * 200) / $sizeNormale->getHeight();
-        
-        $thumb_w = ($sizeNormale->getWidth()*$to_pourcent_200px)/100; 
-        $thumb_h = 200;
-        $sizeThumb = new ImageRect($thumb_w, $thumb_h);
-        $thumb_name = $baseName.'-thumb.'.$extension;
-        
-        $thumb_destination = new Image($thumb_name, $sizeThumb);
-        self::equalize($image, $thumb_destination);
-        
-        
-        //Creation du thumb carree
-        $sizeThumb_carre = new ImageRect(200, 200, (($thumb_w-200)/2), 1);
-        $thumb_carre_name = $baseName.'-thumb-carre.'.$extension;
-        
-        $images_thumb = array();
-        $img_thumb = new Image($thumb_name);
-        $images_thumb[] = new Image($thumb_carre_name, $sizeThumb_carre);
-        $resize = new ImageResizing($img_thumb, $images_thumb);
-        $resize->build();
-        
-        //creation du minuature du thumb carree
-        $thumb_carre_min_name = $baseName.'-thumb-carre-xs.'.$extension;
-        $thumb_carre_source = new Image($thumb_carre_name);
-        $thumb_carre_destination = new Image($thumb_carre_min_name, ImageRect::getDefaultXsCarreSize());
-        self::equalize($thumb_carre_source, $thumb_carre_destination);
+            $sizeNormale = ImageRect::getDefaultSize();//pour la taille normale => Pour la photo de dimension requise, un copression simple est naicessaire
+            $imageName = $baseName.'.'.$extension;//le nom de l'image de base            
+            $destination = new Image($imageName, $sizeNormale);
+            $images[] = $destination;//image compressee
             
-        //Supression du fichier original
-        if ($remove) {
+            $sizeMin = ImageRect::getDefaultMinuatureSize();//Pour les images minuatures
+            $imageMinName = $baseName.'-min.'.$extension;
+            $min_destination = new Image($imageMinName, $sizeMin);
+            $images[] = $min_destination;//image min
+
+            //Creation du thumb normal => Le principe de base est de calculer le pourcentage de la hoteur de l'image, qui veau 200px
+            $to_pourcent_200px = (100 * 200) / $sizeNormale->getHeight();
+            $thumb_w = ($sizeNormale->getWidth()*$to_pourcent_200px)/100;
+            $thumb_h = 200;
+            
+            $sizeThumb = new ImageRect($thumb_w, $thumb_h);//
+            $thumb_name = $baseName.'-thumb.'.$extension;
+            $thumb_destination = new Image($thumb_name, $sizeThumb);
+            $images[] = $thumb_destination;//image thumb
+            
+            foreach ($images as $img) {
+                self::equalize($image, $img);
+            }
+            
+            //Creation du thumb carree
+            $sizeThumb_carre = new ImageRect(200, 200, (($thumb_w-200)/2), 1);
+            $thumb_carre_name = $baseName.'-thumb-carre.'.$extension;
+            
+            $images_thumb = array();
+            $img_thumb = new Image($thumb_name);
+            $images_thumb[] = new Image($thumb_carre_name, $sizeThumb_carre);
+            $resize = new ImageResizing($img_thumb, $images_thumb);
+            $resize->build();
+            
+            //creation du minuature du thumb carree
+            $thumb_carre_min_name = $baseName.'-thumb-carre-xs.'.$extension;
+            $thumb_carre_source = new Image($thumb_carre_name);
+            $thumb_carre_destination = new Image($thumb_carre_min_name, ImageRect::getDefaultXsCarreSize());
+            self::equalize($thumb_carre_source, $thumb_carre_destination);
+        } else {
+            foreach ($sizes as $rect) {
+                $name = "{$baseName}-{$rect->getWidth()}x{$rect->getHeight()}.{$extension}";
+                $img = new Image($name, $rect);
+                self::equalize($image, $img);
+            }
+        }
+
+        if ($remove) {//Supression du fichier original
 	        @unlink($image->getFileName());        	
         }
     }
@@ -188,10 +188,9 @@ class ImageResizing extends ImageBuilder
     /**
      * Dimesionnement d'une photo de profile
      * @param Image $image
-     * @param ImageRect $imageRect
      * @param bool $remove supprimer l'original????
      */
-    public static function profiling(Image $image, ?ImageRect $imageRect = null, $remove=true) : void{
+    public static function profiling(Image $image, $remove=true) : void{
         $matches = array();
         
         if (!$image->fileExists()) {
@@ -201,7 +200,7 @@ class ImageResizing extends ImageBuilder
         $baseName = '';
         $extension = '';
         
-        if (preg_match('#^(.+)-reel\.(png|PNG|jpg|jpeg|JPG|JPEG)$#', $image->getFileName(), $matches)) {
+        if (preg_match('#^(.+)-reel\.(png|PNG|jpg|JPG|jpeg|JPEG)$#', $image->getFileName(), $matches)) {
             $baseName = $matches[1];
             $extension = $matches[2];
         }else {
@@ -242,7 +241,6 @@ class ImageResizing extends ImageBuilder
             $s_port = ImageRect::getA6Portrait();
             $size = new ImageRect($s_port->getWidth(), $s_port->getWidth());
         }
-
         
         if ($width >= ($height + $w_pourcent20) ) {//Pour les image en paysage
             $carreSize = new ImageRect($size->getHeight(), $size->getHeight());
@@ -252,7 +250,6 @@ class ImageResizing extends ImageBuilder
         
         $imageResized = new Image($imageName, $size);
         self::equalize($image, $imageResized);//Redimensionnement de l'image
-        
 
         //Generation des minuature de l'image
         $requieredBigName = $baseName.'-lg.'.$extension;//Profil grand carree
@@ -286,9 +283,10 @@ class ImageResizing extends ImageBuilder
     
     
     /***
-     * mise en echel d'une image
-     * @param Image $img_source
-     * @param Image $img_destination
+     * mis en echel d'une image
+     * @param Image $img_source, la source doit exister sur le disque dur
+     * @param Image $img_destination, information des dimensions de l'image a generer
+     * @throws PHPBackendException
      */
     protected static function equalize(Image $img_source, Image $img_destination) : void{
         if ($img_source->getType() == Image::IMAGE_TYPE_PNG) {
@@ -296,13 +294,13 @@ class ImageResizing extends ImageBuilder
         }else {
             $source = imagecreatefromjpeg($img_source->getFileName());
         }
+        
         $size = $img_destination->getSize();
         $destination = imagecreatetruecolor($size->getWidth(), $size->getHeight());
         $statut = imagecopyresampled($destination, $source, 0, 0, 0, 0, $size->getWidth(), $size->getHeight(), $img_source->getSize()->getWidth(), $img_source->getSize()->getHeight());
         if (!$statut) {
             throw new PHPBackendException('Echec d\'execution de l\'operation. une erreur est survenue lors de construction de l\'image.');
-        }
-        
+        }        
         
         //Ecriture de l'image sur le dique dur
         if ($img_source->getType() == Image::IMAGE_TYPE_PNG){
